d6a7f89f4833fc36e679a279caecde2ccffa1de1
Evan LaRusso <lalalarusso@gmail.com>
2019-04-01 04:35:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ function happyHolidays() {
 function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`
 }
+
+function happyHolidaysTo(holiday, name) {
+  return `Happy ${holiday}, ${name}!`
+}
